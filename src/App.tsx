729d86b31a2168133e1cf7a9bed1f8db2ac0fafa
fakitayran <aridurufakitayran@gmail.com>
2024-08-22 21:44:04 +0300

    feat: add tailwind, init shadcn, global variables, fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { useAccount, useReadContract } from "wagmi";
-import { PropertyContract } from "./smart-contracts/Property";
-import { config } from "./config/wagmiConfig";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Map, ShoppingBag } from "lucide-react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useAccount, useReadContract } from "wagmi";
 import {
   simulateContract,
   waitForTransactionReceipt,
   writeContract,
 } from "wagmi/actions";
-import { useState } from "react";
-import {
-  Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-} from "@mui/material";
+import { config } from "./config/wagmiConfig";
+import { PropertyContract } from "./smart-contracts/Property";
+
 import { formatEther } from "viem";
 
 type PropertyDetails = {
@@ -39,6 +39,10 @@ type PropertyDetails = {
 };
 
 const useQuery = () => {
+  // const search = useLocation().search
+  // useCallback(() => {
+  //   return new URLSearchParams(search);
+  // }, [search])
   return new URLSearchParams(useLocation().search);
 };
 function abbreviateAddress(address: string): string {
@@ -87,100 +91,97 @@ function App() {
     }
   };
   return (
-    <>
-      <div>
-        <a
-          href="https://vitejs.dev"
-          target="_blank">
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank">
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Apartment Sale</h1>
-      <p>
+    <main className="grid place-items-center h-screen max-w-6xl mx-auto"
+    >
+      <section className="container mx-auto flex flex-col gap-8">
+        <h1 className="text-transparent bg-gradient-to-br from-primary/20 to-primary bg-clip-text text-[5rem] text-center">Apartment Sale</h1>
+
         {!!property && (
-          <TableContainer component={Paper}>
-            <Table
-              sx={{ minWidth: 650 }}
-              aria-label="apartment details table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Title Number</TableCell>
-                  <TableCell>Address</TableCell>
-                  <TableCell>On Sale</TableCell>
-                  <TableCell>On Lease</TableCell>
-                  <TableCell>Sell Price (MATIC)</TableCell>
-                  <TableCell>Sell Price (Sterlin)</TableCell>
-                  <TableCell>Floor Area</TableCell>
-                  <TableCell>Current Owner</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                <TableRow>
-                  <TableCell>
-                    {(property as PropertyDetails).titleNumber.toString()}
-                  </TableCell>
-                  <TableCell>
-                    {(property as PropertyDetails).propertyAddress.toString()}
-                  </TableCell>
-                  <TableCell>
-                    {(property as PropertyDetails).onSale ? "Yes" : "No"}
-                  </TableCell>
-                  <TableCell>
-                    {(property as PropertyDetails).onLease ? "Yes" : "No"}
-                  </TableCell>
-                  <TableCell>
-                    {formatEther((property as PropertyDetails).sellPrice)}
-                  </TableCell>
-                  <TableCell>
-                    {(property as PropertyDetails).sterlinPrice}
-                  </TableCell>
-                  <TableCell>
-                    {`${
-                      (property as PropertyDetails).squareMeters
+          <Table>
+            <TableCaption className="font-light text-xs">A list of available apartments. More data will be imported.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Title Number</TableHead>
+                <TableHead>Address</TableHead>
+                <TableHead>On Sale</TableHead>
+                <TableHead>On Lease</TableHead>
+                <TableHead>Sell Price (MATIC)</TableHead>
+                <TableHead>Sell Price (Sterlin)</TableHead>
+                <TableHead>Floor Area</TableHead>
+                <TableHead className="text-right">Current Owner</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">
+                  {(property as PropertyDetails).titleNumber.toString()}
+                </TableCell>
+                <TableCell>
+                  {(property as PropertyDetails).propertyAddress.toString()}
+                </TableCell>
+                <TableCell>
+                  {(property as PropertyDetails).onSale ? "Yes" : "No"}
+                </TableCell>
+                <TableCell>
+                  {(property as PropertyDetails).onLease ? "Yes" : "No"}
+                </TableCell>
+                <TableCell>
+                  {formatEther((property as PropertyDetails).sellPrice)}
+                </TableCell>
+                <TableCell>
+                  {(property as PropertyDetails).sterlinPrice}
+                </TableCell>
+                <TableCell>
+                  {`${(property as PropertyDetails).squareMeters
                     } Square meters`}
-                  </TableCell>
+                </TableCell>
 
-                  <TableCell>
-                    {abbreviateAddress((property as PropertyDetails).owner)}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
+                <TableCell className="text-right">
+                  {abbreviateAddress((property as PropertyDetails).owner)}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                {Array.from({ length: 8 }).map((_, idx) => {
+                  return (
+                    <TableCell key={idx}>
+                      <Skeleton className="w-[100px] h-[20px] rounded-full" />
+                    </TableCell>
+                  )
+                })}
+              </TableRow>
+            </TableBody>
+          </Table>
+
         )}
-      </p>
 
-      <button>See on Maps</button>
+        <div className="flex w-full items-center justify-between gap-4">
 
-      <div className="card">
-        {isConnected ? (
-          <Button
-            disabled={
-              isLoading ||
-              isError
-            }
-            onClick={handleBuy}>
-            Buy
+          <Button variant="outline" className="w-1/2">
+            <Map className="size-4 mr-2 text-primary" />
+            See on Maps
           </Button>
-        ) : (
-          <w3m-connect-button />
-        )}
-      </div>
-    </>
-  );
+
+          {isConnected ? (
+            <Button
+              className="w-1/2"
+              disabled={
+                isLoading ||
+                isError
+              }
+              onClick={handleBuy}>
+              <ShoppingBag className="size-4 mr-2 text-primary-foreground" />
+              Buy
+            </Button>
+          ) : (
+            <Button className="p-4 border w-1/2" id="connectBtn">
+              <w3m-connect-button label="Connect your wallet" loadingLabel="Loading.." size="mdl" />
+            </Button>
+          )}
+        </div>
+      </section>
+
+    </main>
+  )
 }
 
 export default App;
